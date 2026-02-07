5f3bf78c64d5710e0ff6ff3c8927ab2f89aef099
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-02-08 03:59:18 +0530

    Done a practice set

--- 02 Express.js Intro/11. Dynamic UI using EJS/router/serviceRouter.js
+++ 02 Express.js Intro/11. Dynamic UI using EJS/router/serviceRouter.js
@@ -8,20 +8,19 @@ const rootDir = require('../utils/pathUtil');
 const serviceRouter = express.Router();
 
 serviceRouter.get("/add-home", (req, res, next) => {
-  res.render('add-home', {pageTitle: 'Add New Home'});
+  res.render('add-home', { pageTitle: 'Add New Home' });
 });
 
 // An array to store home listings (in-memory)
 const registeredHomes = [];
 
 serviceRouter.post("/add-home", (req, res, next) => {
-  res.render('home-added-success', {pageTitle: 'Home Added Successfully'});
-  registeredHomes.push({houseName: req.body.houseName});
+  res.render('home-added-success', { pageTitle: 'Home Added Successfully' });
+  registeredHomes.push({ houseName: req.body.houseName, location: req.body.locationName });
   console.log(req.body);
-  console.log(registeredHomes);
 });
 
-exports.serviceRouter = serviceRouter;  
+exports.serviceRouter = serviceRouter;
 exports.registeredHomes = registeredHomes;
 
 // module.exports = {registeredHomes, serviceRouter}; // Alternative way to export multiple things
