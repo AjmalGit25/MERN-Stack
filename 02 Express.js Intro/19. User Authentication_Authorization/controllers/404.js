37e15cc5bc811b8c5ea1a402e0904fcf1a5467c2
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-03-10 23:47:26 +0530

    Chapter 19. done! Learned the implementation of User Authentication and Authorization

--- 02 Express.js Intro/19. User Authentication_Authorization/controllers/404.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    currPage: '404',
-    isLoggedIn: req.isLoggedIn
-  });
-};
-
-// Single export of the 404 controller function.
