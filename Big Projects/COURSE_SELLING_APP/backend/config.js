fcbbb3b112b79cbf292de99a8a7e1c53672259e9
Md Ajmal Hussain <ajmalashrafi2016@gmail.com>
2026-04-03 01:25:20 +0530

    Removed Stripe secret key

--- Big Projects/COURSE_SELLING_APP/backend/config.js
+++ Big Projects/COURSE_SELLING_APP/backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
-const STRIPE_SECRET_KEY = "sk_test_51THTwWK8kgtlHxktVzwFGRGdfTxh8EzKsm02rwuVmlEJIg8gQDDlvDDnHqo2OAU6GqlAYGhAQofGrC2r0ONrGGno00JZApk3w2";
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
 export default {
   JWT_USER_PASSWORD, 
